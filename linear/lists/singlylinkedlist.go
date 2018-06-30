d34cdc5124e5a076a8531ace361dbbfee8debc69
Anthony Abeo <anthonyabeo@gmail.com>
2018-06-30 02:18:42 +0000

    singlylinked list implemented

---
+++ linear/lists/singlylinkedlist.go
@@ -0,0 +1,97 @@
+package list
+
+// usual imports
+import "fmt"
+
+
+/// This is Node implementation. The node is private
+/// outside this file since it starts with lowercase
+/// Here a struct data structure to hold the items
+/// of the Node. These include the item and the next
+type node struct {
+    item interface{}
+    next *node
+}
+
+// returns a pointer to the item in the node
+func (n node) Item() interface{} {
+    return n.item
+}
+
+// returns a pointer to the next item in the node
+func (n node) Next() *node {
+    return n.next
+}
+
+
+/// This is SinglyLikedList implementation
+/// Here a struct data structure to hold the items
+/// of the list. These include the number of items,
+/// the head and the tail of the list
+type SinglyLinkedList struct {
+    numItems int
+    head *node
+    tail *node
+}
+
+// Add a new item to the front of the list
+func (sll *SinglyLinkedList) AddFirst(e interface{}) {
+    sll.head = &node{item: e, next: sll.head}
+    if sll.numItems == 0 {
+        sll.tail = sll.head
+    }
+    sll.numItems += 1
+}
+
+// returns the first element from the list
+func (sll SinglyLinkedList) First() interface{}  {
+    return sll.head.item
+}
+
+// Add a new item to the end of the list
+func (sll *SinglyLinkedList) AddLast(e interface{}) {
+    newNode := &node{item: e, next: nil}
+    if sll.Empty() {
+        sll.head = newNode
+    } else {
+        sll.tail.next = newNode
+    }
+
+    sll.tail = newNode
+    sll.numItems += 1
+}
+
+// returns the first element from the list
+func (sll SinglyLinkedList) Last() interface{} {
+    return sll.tail.item
+}
+
+// Returns the number of items in the list
+func (sll SinglyLinkedList) Size() int {
+    return sll.numItems
+}
+
+// returns a boolean indicating whether or not
+// the list is empty or not
+func (sll SinglyLinkedList) Empty() bool  {
+    return sll.numItems == 0
+}
+
+func (sll SinglyLinkedList) RemoveFirst() interface{} {
+    if sll.Empty(){
+        return nil
+    }
+    first := sll.First()
+    sll.head = sll.head.Next()
+    sll.numItems--
+
+    if sll.numItems == 0 {
+        sll.tail = nil
+    }
+
+    return first
+}
+// Returns a string representation of the list
+func (sll *SinglyLinkedList) String() string  {
+    return fmt.Sprintf("<%v>", sll.numItems)
+}
---
+++ linear/tests/singlylinkedlist_test.go
@@ -0,0 +1,61 @@
+package tests
+
+import (
+	// Built-in packages
+	"fmt"
+	"testing"
+
+	// User defined packages
+	"go-collections/linear/lists"
+)
+
+var sll = new(list.SinglyLinkedList)
+
+func TestToString(t *testing.T) {
+	fmt.Println(sll.String())
+}
+
+func TestSize(t *testing.T) {
+	if sll.Size() != 0 {
+		t.Error("Expected size =", 0)
+	}
+}
+
+func TestAddFirst(t *testing.T) {
+	sll.AddFirst(5)
+	sll.AddFirst(17)
+	sll.AddFirst(3)
+	fmt.Println("Number of items: ", sll.Size())
+}
+
+func TestFirst(t *testing.T)  {
+	if sll.First() != 3 {
+		t.Error("Expected first =", 3)
+	}
+}
+
+func TestAddLast(t *testing.T) {
+	sll.AddLast(99)
+	fmt.Println("Number of items: ", sll.Size())
+}
+
+func TestLast(t *testing.T)  {
+	if sll.Last() != 99 {
+		t.Error("Expected first =", 99)
+	}
+}
+
+func TestRemoveFirst(t *testing.T)  {
+	first := sll.RemoveFirst()
+	if first != 3 && sll.First() != 17 {
+		t.Error("Expected first =", 17)
+	}
+}
+
+
+
+
+
+
+
+
