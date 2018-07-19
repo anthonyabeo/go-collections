a8eb1b4004bade0a6b3210cb0a15a3ddc990a3aa
Anthony Abeo <anthonyabeo@gmail.com>
2018-07-19 16:53:53 +0000

    add a concurrent counter data structure that uses the singleton design pattern

---
+++ counter/counter.go
@@ -0,0 +1,49 @@
+package counter
+
+import (
+	"sync"
+	"fmt"
+)
+
+var instance *Counter
+
+type Counter struct {
+	value int
+	lock *sync.Mutex
+}
+
+func GetInstance() *Counter {
+	if instance == nil {
+		instance = &Counter{
+			0,
+			new(sync.Mutex),
+		}
+	}
+
+	return instance
+}
+
+func (c *Counter) Increment()  {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	c.value++
+	fmt.Printf("Incrementing: %d\n", c.value)
+}
+
+func (c *Counter) Decrement()  {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	c.value--
+	fmt.Printf("Decrementing: %d\n", c.value)
+}
+
+func (c *Counter) GetValue() int {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	var val = c.value
+
+	return val
+}
